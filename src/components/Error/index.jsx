598632ad2017e3081cc6f51139994882b7e03051
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-05-15 17:06:13 +0200

    Style of the 404 page

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
+import styles from '../../styles/error.module.css'
 
 function Error() {
     return (
-        <section>
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
+        <section className={styles.error}>
+            <div className={styles.message}>
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
             <Link to="/">Retourner sur la page d'accueil</Link>
         </section>
     )
