fc8e7151829606862380f5948f8120a1e77323ad
Sandra Petereau <sandra.petereau@outlook.fr>
2024-05-31 16:04:29 +0200

    feat(footer): move style to module

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,43 +3,45 @@ import logo from '../../images/logo.png'
 import styles from './styles.module.css'
 
 function Header() {
-    let url = useLocation()
-    return (
-        <header className={`${styles.header}`}>
-            <div className={`${styles.header__container}`}>
-                <Link to="/">
-                    <img
-                        src={logo}
-                        alt="Mot Kasa avec une maison à la place du a"
-                        className={`${styles.header__logo}`}
-                    />
-                </Link>
+  let url = useLocation()
+  return (
+    <header className={`${styles.header}`}>
+      <div className={`${styles.header__container}`}>
+        <Link to="/">
+          <img
+            src={logo}
+            alt="Mot Kasa avec une maison à la place du a"
+            className={`${styles.header__logo}`}
+          />
+        </Link>
 
-                <nav>
-                    <ul>
-                        <li>
-                            <Link
-                                to="/"
-                                className={url.pathname === '/' ? `${styles.header__active}` : ''}
-                            >
-                                Accueil
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                to="/about"
-                                className={
-                                    url.pathname === '/about' ? `${styles.header__active}` : ''
-                                }
-                            >
-                                À propos
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-    )
+        <nav>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={
+                  url.pathname === '/' ? `${styles.header__active}` : ''
+                }
+              >
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={
+                  url.pathname === '/about' ? `${styles.header__active}` : ''
+                }
+              >
+                À propos
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
