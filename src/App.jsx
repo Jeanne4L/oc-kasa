14a76b178b3161441109a3bf657376f924d88670
Sandra Petereau <sandra.petereau@outlook.fr>
2024-05-31 18:03:28 +0200

    fix(scroll): create component to reset scroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Error from './pages/Error/index'
 import Rental from './pages/Rental'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<Error />} />
-                <Route path="/rental/:id" element={<Rental />} />
-            </Routes>
-            <Footer />
-        </Router>
-    )
+  return (
+    <Router>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/rental/:id" element={<Rental />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
